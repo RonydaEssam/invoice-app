e4f584d83cc8b25e5654775369f406f06ada34c2
Ronyda Essam <ronyda988@gmail.com>
2026-06-23 13:04:30 +0300

    add orders page & connect api endpoints for getAll & delete

--- client/src/pages/OrdersPage.tsx
+++ client/src/pages/OrdersPage.tsx
@@ -1,5 +1,68 @@
+import { useEffect, useState } from "react"
+import { Link, useNavigate } from "react-router-dom"
+import { deleteData } from "../api/transformData"
+
+interface Order {
+    id: number
+    clientId: number
+    client: {
+        name: string
+    }
+    date: Date
+    status: string
+    orderItems: {
+        serviceId: number
+        quantity: number
+        service: {
+            name: string
+            price: number
+        }
+    }[]
+
+}
+
 function OrdersPage() {
-    return <h1>Orders Page</h1>
+    const [orders, setOrders] = useState<Order[]>([]);
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        fetch('http://localhost:3000/orders')
+            .then(response => response.json())
+            .then(data => setOrders(data))
+    }, [])
+
+    function deleteOrder(id: number) {
+        deleteData("orders", Number(id))
+            .then(() => setOrders(orders.filter(order => order.id !== id)))
+            .catch(error => alert(error.message))
+    }
+
+    return (
+        <div>
+            <div className="list-header">
+                <h1>Orders</h1>
+                <Link to='/orders/new' className="btn-primary">+ Add Order</Link>
+            </div>
+
+            <div className="card-list">
+                {
+                    orders.map(order => (
+                        <div className="card" key={order.id}>
+                            <div>
+                                <p>Client: {order.client.name}</p>
+                                <p>Order: {order.orderItems.map(item => `${item.service.name} * ${item.quantity}`).join(', ')}</p>
+                                <p>Status: {order.status}</p>
+                            </div>
+
+                            <button onClick={() => deleteOrder(order.id)}>delete</button>
+                            <button onClick={() => navigate(`edit/${order.id}`)}>edit</button>
+                        </div>
+                    ))
+                }
+            </div>
+        </div>
+
+    )
 }
 
 export default OrdersPage
