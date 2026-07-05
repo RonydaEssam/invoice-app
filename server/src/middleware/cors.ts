71f00800af080b8a85276052118179eacd661488
Ronyda Essam <ronyda988@gmail.com>
2026-07-05 17:47:39 +0300

    add netlify URL to corsMiddleWare

--- server/src/middleware/cors.ts
+++ server/src/middleware/cors.ts
@@ -1,5 +1,10 @@
 import cors from "cors";
 
-const corsMiddleware = cors({ origin: 'http://localhost:5173' });
+const corsMiddleware = cors({
+    origin: [
+        'http://localhost:5173',
+        'https://your-app.netlify.app'
+    ]
+});
 
 export { corsMiddleware };
