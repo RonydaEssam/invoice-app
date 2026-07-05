bc7e3154131a0ba0813242b96cd57a77dff677f0
Ronyda Essam <ronyda988@gmail.com>
2026-07-05 18:11:49 +0300

    update netlify url

--- server/src/middleware/cors.ts
+++ server/src/middleware/cors.ts
@@ -3,7 +3,7 @@ import cors from "cors";
 const corsMiddleware = cors({
     origin: [
         'http://localhost:5173',
-        'https://storied-dango-1049ec.netlify.app'
+        'https://invoice-generator-app1.netlify.app'
     ]
 });
 
