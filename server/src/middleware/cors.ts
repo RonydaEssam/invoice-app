be03d721827bccd942e76ceb4aec16b4ba45c19b
Ronyda Essam <ronyda988@gmail.com>
2026-07-05 18:23:28 +0300

    update url

--- server/src/middleware/cors.ts
+++ server/src/middleware/cors.ts
@@ -3,7 +3,7 @@ import cors from "cors";
 const corsMiddleware = cors({
     origin: [
         'http://localhost:5173',
-        'https://invoice-generator-app1.netlify.app'
+        'https://invoice-app-lilac-theta.vercel.app'
     ]
 });
 
