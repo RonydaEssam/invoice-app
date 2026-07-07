71989f1b63fbda8ec8cb8a5da0a426352608267a
Ronyda Essam <ronyda988@gmail.com>
2026-07-07 18:15:52 +0300

    fix: make it mobile responsive

--- client/src/components/shared/Navbar.tsx
+++ client/src/components/shared/Navbar.tsx
@@ -1,18 +1,29 @@
 import { Link } from "react-router-dom";
 import './Navbar.css'
 import { useTheme } from "../../context/ThemeContext";
+import { useState } from "react";
 
 function Navbar() {
     const { theme, toggleTheme } = useTheme();
+    const [menuOpen, setMenuOpen] = useState(false);
 
     return (
         <nav className="navbar">
+            <button className="navbar-menu-toggle" onClick={() => setMenuOpen(!menuOpen)}>
+                <span></span>
+                <span></span>
+                <span></span>
+            </button>
+
             <span className="navbar-brand">Invoice App</span>
-            <Link className="navbar-link" to={'/'}>Home</Link>
-            <Link className="navbar-link" to={'/clients'}>Clients</Link>
-            <Link className="navbar-link" to={'/services'}>Services</Link>
-            <Link className="navbar-link" to={'/orders'}>Orders</Link>
-            <Link className="navbar-link" to={'/invoices'}>Invoices</Link>
+
+            <div className={`navbar-menu ${menuOpen ? 'open' : ''}`}>
+                <Link className="navbar-link" to={'/'} onClick={() => setMenuOpen(false)}>Home</Link>
+                <Link className="navbar-link" to={'/clients'} onClick={() => setMenuOpen(false)}>Clients</Link>
+                <Link className="navbar-link" to={'/services'} onClick={() => setMenuOpen(false)}>Services</Link>
+                <Link className="navbar-link" to={'/orders'} onClick={() => setMenuOpen(false)}>Orders</Link>
+                <Link className="navbar-link" to={'/invoices'} onClick={() => setMenuOpen(false)}>Invoices</Link>
+            </div>
 
             <label className="theme-switch">
                 <input
